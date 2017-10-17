25b7d1d1892c495d9a8ad04770b031417f2b3183
angelwsin <angelwsin@hotmail.com>
2017-10-17 17:21:43 +0800

    angelwsin
    idea 快捷键

--- java-jdk9/java-jdk9-umodule/src/main/java/com/app/App.java
+++ java-jdk9/java-jdk9-umodule/src/main/java/com/app/App.java
@@ -4,8 +4,31 @@ package com.app;
  *   eclispe    idea
  *   alt+/      ctrl+space
  *   syso       sout+tab
+ *   ctrl+h     ctrl+n/Ctrl+Shift+N/shift+shift
+ *   ctrl+t     ctrl+h   类
+ *   ctrl+f     ctrl+f
+ *   ctrl+g     ctrl+alt+h
+ *   ctrl+d     ctrl+x
+ *   ctrl+t     ctrl+alt+b  方法
+ *   ctrl+o      ctrl+f12
+ *
+ *
+ *
  *
  */
+
+
+/*Ø  Top #10切来切去：Ctrl+Tab
+        Ø  Top #9选你所想：Ctrl+W
+        Ø  Top #8代码生成：Template/Postfix +Tab
+        Ø  Top #7发号施令：Ctrl+Shift+A
+        Ø  Top #6无处藏身：Shift+Shift
+        Ø  Top #5自动完成：Ctrl+Shift+Enter
+        Ø  Top #4创造万物：Alt+Insert
+        太难割舍，前三名并列吧！
+        Ø  Top #1智能补全：Ctrl+Shift+Space
+        Ø  Top #1自我修复：Alt+Enter
+        Ø  Top #1重构一切：Ctrl+Shift+Alt+T*/
 public class App 
 {
     public static void main( String[] args )
